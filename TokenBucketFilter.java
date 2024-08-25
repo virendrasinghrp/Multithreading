64d88300312a92ce01b42f6b36c76a9223ad6971
Virendra Singh Rajpurohit <virendrasinghrp@gmail.com>
2024-08-25 18:12:02 +0530

    Added problem description

--- TokenBucketFilter.java
+++ TokenBucketFilter.java
@@ -1,6 +1,10 @@
 import java.util.HashSet;
 import java.util.Set;
 
+// This is an actual interview question asked at Uber and Oracle.
+// Imagine you have a bucket that gets filled with tokens at the rate of 1 token per second. The bucket can hold a maximum of N tokens.
+// Implement a thread-safe class that lets threads get a token when one is available. If no token is available, then the token-requesting threads should block.
+// The class should expose an API called `getToken()` that various threads can call to get a token
 class Scratch {
     public static void main(String[] args) throws InterruptedException {
         TokenBucketFilter.runTestMaxToken(3);
